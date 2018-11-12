ff275e007ab834b1744238152149427e724ff521
Nicolas Vergne <nicolasvergne88@gmail.com>
2018-11-12 13:12:20 +0000

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,17 +14,14 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*s1_cpy;
-	unsigned char	*s2_cpy;
+	size_t	i;
 
 	i = 0;
-	s1_cpy = (unsigned char *)s1;
-	s2_cpy = (unsigned char *)s2;
-	if (n == 0)
-		return (0);
-	while (s1_cpy[i] == s2_cpy[i]
-		&& i < n - 1 && s1_cpy[0] != 0 && s2_cpy[0] != 0)
+	while (n--)
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
-	return (s1_cpy[i] - s2_cpy[i]);
+	}
+	return (0);
 }
