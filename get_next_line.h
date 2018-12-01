3394a5e362dacc8e1dab1a7d76f6d71d61c0ab2a
nicovrg <nicolasvergne88@gmail.com>
2018-12-01 17:26:45 +0100

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: nivergne <nivergne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 17:10:20 by nivergne          #+#    #+#             */
-/*   Updated: 2018/11/28 19:49:42 by nivergne         ###   ########.fr       */
+/*   Updated: 2018/12/01 17:26:24 by nivergne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
+# include "libft.h"
 
-# define BUFF_SIZE 10000
+# define BUFF_SIZE 4096
 
 int			get_next_line(const int fd, char **line);
 
