27517061244e88962524a880d1ad3d7cc7072ba0
Nicolas VERGNE <nivergne@e1r4p14.42.fr>
2019-07-09 06:15:42 +0200

    gnl buff -> 1

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nivergne <nivergne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 17:10:20 by nivergne          #+#    #+#             */
-/*   Updated: 2019/05/15 21:36:46 by nivergne         ###   ########.fr       */
+/*   Updated: 2019/07/09 06:15:31 by nivergne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "libft.h"
 
-# define BUFF_SIZE 10000
+# define BUFF_SIZE 1
 
 int			get_next_line(const int fd, char **line);
 
